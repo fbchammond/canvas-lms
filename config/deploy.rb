986f45758c62dbcdb4e0d93a14aba659b211f058
Jack Christensen <jackc@hylesanderson.edu>
2012-07-09 14:10:53 -0500

    Modify deployment for HB

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,14 +4,14 @@ set :application, "canvas-lms"
 
 set :gateway, 'root@dog.hylesanderson.edu'
 set :scm, :git
-set :repository,  "ssh://git@dev.fbchammond.com/var/git/vendor/canvas-lms.git"
-set :branch, "hac"
+set :repository,  "https://github.com/fbchammond/canvas-lms.git"
+set :branch, "hb"
 set :repository_cache, "git_cache"
 set :deploy_via, :remote_cache
-set :deploy_to, "/var/www/canvas"
+set :deploy_to, "/var/www/hb_canvas"
 
-role :web, "root@newt.hylesanderson.edu", "root@ox.hylesanderson.edu" # Your HTTP server, Apache/etc
-role :app, "root@newt.hylesanderson.edu", "root@ox.hylesanderson.edu" # This may be the same as your `Web` server
+role :web, "root@newt.hylesanderson.edu" # Your HTTP server, Apache/etc
+role :app, "root@newt.hylesanderson.edu" # This may be the same as your `Web` server
 role :db,  "root@newt.hylesanderson.edu", :primary => true # This is where Rails migrations will run
 
 set :use_sudo, false
@@ -23,22 +23,22 @@ after "deploy:update_code" do
   
   run "ln -s #{shared_path}/files #{release_path}/tmp/files"
   
-  run "ln -s #{shared_path}/google2248bfbba38f2b28.html #{release_path}/public/google2248bfbba38f2b28.html"
+  #run "ln -s #{shared_path}/google2248bfbba38f2b28.html #{release_path}/public/google2248bfbba38f2b28.html"
   
   # The Facebook icon is missing so symlink on deploy -- when the symlink fails it presumably means that the problem is fixed
-  run "ln -s #{release_path}/public/images/email_big.png #{release_path}/public/images/conversation_message_icon.png"
+  #run "ln -s #{release_path}/public/images/email_big.png #{release_path}/public/images/conversation_message_icon.png"
   
-  run "chown canvas:canvas #{release_path}/config/environment.rb"
-  run "chown canvas:canvas #{release_path}/db"
-  run "chown canvas:canvas #{release_path}/tmp"
+  run "chown hb_canvas:hb_canvas #{release_path}/config/environment.rb"
+  run "chown hb_canvas:hb_canvas #{release_path}/db"
+  run "chown hb_canvas:hb_canvas #{release_path}/tmp"
   
-  run "cd #{release_path} && RAILS_ENV=assets bundle exec rake canvas:compile_assets"
-  run "cd #{release_path} && RAILS_ENV=assets bundle exec rake canvas:compress_assets"
+  run "cd #{release_path} && RAILS_ENV=assets bundle exec rake hb_canvas:compile_assets"
+  run "cd #{release_path} && RAILS_ENV=assets bundle exec rake hb_canvas:compress_assets"
 end
 
 after "deploy:symlink" do
   # restart worker
-  run "service canvas_init restart"
+  run "service hb_canvas_init restart"
 end
 
 namespace :deploy do
