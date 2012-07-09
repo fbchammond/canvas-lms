74db5ad4a51e155cc5d3d27f57f8cb239f0033a5
Jack Christensen <jackc@hylesanderson.edu>
2012-07-09 16:02:30 -0500

    use github instead of internal git server to deploy from

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :application, "canvas-lms"
 
 set :gateway, 'root@dog.hylesanderson.edu'
 set :scm, :git
-set :repository,  "ssh://git@dev.fbchammond.com/var/git/vendor/canvas-lms.git"
+set :repository,  "https://github.com/fbchammond/canvas-lms.git"
 set :branch, "hac"
 set :repository_cache, "git_cache"
 set :deploy_via, :remote_cache
@@ -20,18 +20,18 @@ after "deploy:update_code" do
   %w{ amazon_s3 cache_store database delayed_jobs domain file_store outgoing_mail security }.each do |f|
     run "ln -s #{shared_path}/#{f}.yml #{release_path}/config/#{f}.yml"
   end
-  
+
   run "ln -s #{shared_path}/files #{release_path}/tmp/files"
-  
+
   run "ln -s #{shared_path}/google2248bfbba38f2b28.html #{release_path}/public/google2248bfbba38f2b28.html"
-  
+
   # The Facebook icon is missing so symlink on deploy -- when the symlink fails it presumably means that the problem is fixed
   run "ln -s #{release_path}/public/images/email_big.png #{release_path}/public/images/conversation_message_icon.png"
-  
+
   run "chown canvas:canvas #{release_path}/config/environment.rb"
   run "chown canvas:canvas #{release_path}/db"
   run "chown canvas:canvas #{release_path}/tmp"
-  
+
   run "cd #{release_path} && RAILS_ENV=assets bundle exec rake canvas:compile_assets"
   run "cd #{release_path} && RAILS_ENV=assets bundle exec rake canvas:compress_assets"
 end
